6b451f18f42128e4f3002100a6918b133dc43177
Stanislav Mursalov <shamaner.17@gmail.com>
2021-01-22 16:41:45 +0300

    Shaman

--- front/page.go
+++ front/page.go
@@ -1,4 +1,4 @@
-package front
+	package front
 
 import (
 	"github.com/massarakhsh/lik/likdom"
--- js/data.js
+++ js/data.js
@@ -1,37 +1,15 @@
 
-export function getSys(table, sys, callback) {
-    likref_get_sys(table, sys, callback);
-}
-
-export function getKey(table, key, val, callback) {
-    likref_get_all(table, (elms) => {
-        let list = [];
-        if (elms) {
-            for (var sys in elms) {
-                let elm = elms[sys];
-                if (!elm) {
-                } else if (!key) {
-                    list.push(elm);
-                } else if (elm[key] == val) {
-                    list.push(elm);
-                }
-            }
-        }
-        callback(list);
-    });
-}
-
 export const st_exist = 0x1;
 export const st_online = 0x2;
 export const st_offline = 0x4;
 export const st_dynamic = 0x8;
 
 export function getListZone(callback) {
-    getKey('IPZone', null, null, callback);
+    likref_get_keyval('IPZone', null, null, callback);
 }
 
 export function getIPStatus(ip, callback) {
-    getKey('IP', 'IP', ip, (list) => {
+    likref_get_keyval('IP', 'IP', ip, (list) => {
         let status = 0;
         if (list) {
             for (let np = 0; np < list.length; np++) {
